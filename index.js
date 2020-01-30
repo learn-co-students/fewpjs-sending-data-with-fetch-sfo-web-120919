844d17c158a19b8ee98c1f7486a251ee6538ed0e
coronaisme <coronaisme@gmail.com>
2020-01-29 16:13:58 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+  let formData = {
+    name, 
+    email
+  }
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(res => res.json())
+  .then(json => {
+    console.log(json)
+    const body = document.querySelector('body')
+    const div = document.createElement('div')
+    div.textContent = json['id']
+    body.append(div)
+  })
+  .catch(error => {
+    document.body.append("Unauthorized Access")
+  })
+}
